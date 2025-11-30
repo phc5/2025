f68ef24576958b5865fe2501f5479183be83e616
Paul Chong <paulhyunchong@gmail.com>
2025-11-30 13:56:15 -0800

    Adds Globe Gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Experience from "@/components/Experience";
-import GridMotion from "@/components/GridMotion";
 import Hero from "@/components/Hero";
 import Island from "@/components/Island";
 import NavBar from "@/components/NavBar";
 import TechStack from "@/components/TechStack";
-
-const items = [
-  "Item 1",
-  <div key="jsx-item-1">Custom JSX Content</div>,
-  // Add more items as needed
-];
 
 export default function Home() {
   return (
@@ -20,11 +13,6 @@ export default function Home() {
       <Island />
       <main className="min-h-screen w-full items-center justify-between py-8 px-16 bg-white sm:items-start">
         <Hero />
-        <div className="h-svh relative px-4 lg:px-8 overflow-x-hidden">
-          <div className="absolute w-[50%] h-[50%] left-[25%] top-[10%]">
-            <GridMotion items={items} />
-          </div>
-        </div>
         <About />
         <Experience />
         <TechStack />
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,30 +1,15 @@
 import React from "react";
 import Image from "next/image";
+import GlobeGallery from "./GlobeGallery";
 
 export default function About() {
   return (
     <section
       id="about"
-      className="h-svh grid grid-cols-12 gap-4 lg:gap-8 pt-56 pb-28 p-4 lg:px-8"
+      className="h-svh flex justify-center pt-56 pb-28 p-4 lg:px-8"
     >
-      <div className="flex flex-col col-span-12 lg:col-span-5 lg:col-start-3">
-        <h4 className="font-medium uppercase mb-4">
-          <div className="text-[clamp(16px,1.2vw,20px)]">About Me</div>
-        </h4>
-        <p className="text-[clamp(28px,3.5vw,40px)] font-semibold leading-none">
-          Software Engineer with 8+ years of experience shipping web apps and
-          UIs at Meta, Amazon Web Services, and Kelley Blue Book. Currently
-          building developer infrastructure and tools at Meta.
-        </p>
-      </div>
-      <div className="flex flex-col col-span-12 lg:col-span-3 lg:col-start-9">
-        <Image
-          src="/me.jpg"
-          alt="Paul's portrait"
-          width={250}
-          height={250}
-          className="rounded-2xl"
-        />
+      <div className="flex flex-col col-span-12">
+        <GlobeGallery />
       </div>
     </section>
   );
