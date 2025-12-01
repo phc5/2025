88a05f08f996949c506bdd8125194d04fd5d5b52
Paul Chong <paulhyunchong@gmail.com>
2025-11-30 18:27:08 -0800

    Makes TechStack section responsive

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -72,12 +72,141 @@ export default function TechStack() {
 
   return (
     <section className="h-svh flex flex-col items-center pt-28 px-4 lg:px-8">
-      <h2 className="flex justify-center w-full mb-6 lg:mb-8">
+      <h2 className="flex justify-center w-full mb-8">
         <span className="text-[clamp(48px,12vw,200px)] font-bold tracking-tight leading-[0.8] uppercase">
           Tech Stack
         </span>
       </h2>
       <div ref={gridRef} className="relative w-full">
+        <div className="grid grid-cols-1 lg:hidden">
+          <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="grid-item flex items-center justify-center border-b border-neutral-300 group cursor-pointer h-[100px]"
+            aria-label="Visit React website"
+          >
+            <img
+              alt="React"
+              loading="lazy"
+              width="60"
+              height="60"
+              decoding="async"
+              data-nimg="1"
+              className="z-10"
+              src="/react.svg"
+            />
+          </a>
+          <a
+            href="https://graphql.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="grid-item flex items-center justify-center border-b border-neutral-300 group cursor-pointer h-[100px]"
+            aria-label="Visit GraphQL website"
+          >
+            <img
+              alt="Graph QL"
+              loading="lazy"
+              width="50"
+              height="50"
+              decoding="async"
+              data-nimg="1"
+              className="z-10"
+              src="/graphql.svg"
+            />
+          </a>
+
+          <a
+            href="https://www.typescriptlang.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="grid-item flex items-center justify-center border-b border-neutral-300 group cursor-pointer h-[100px]"
+            aria-label="Visit TypeScript website"
+          >
+            <img
+              alt="TypeScript"
+              loading="lazy"
+              width="50"
+              height="50"
+              decoding="async"
+              data-nimg="1"
+              className="z-10"
+              src="/typescript.svg"
+            />
+          </a>
+          <a
+            href="https://nextjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="grid-item flex items-center justify-center border-b border-neutral-300 group cursor-pointer h-[100px]"
+            aria-label="Visit Next.js website"
+          >
+            <img
+              alt="Next.js"
+              loading="lazy"
+              width="70"
+              height="70"
+              decoding="async"
+              data-nimg="1"
+              className="z-10"
+              src="/nextjs.svg"
+            />
+          </a>
+          <a
+            href="https://tailwindcss.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="grid-item flex items-center justify-center border-b border-neutral-300 group cursor-pointer h-[100px]"
+            aria-label="Visit TailwindCSS website"
+          >
+            <img
+              alt="TailwindCSS"
+              loading="lazy"
+              width="50"
+              height="50"
+              decoding="async"
+              data-nimg="1"
+              className="z-10"
+              src="/tailwindcss.svg"
+            />
+          </a>
+          <a
+            href="https://supabase.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="grid-item flex items-center justify-center border-b border-neutral-300 group cursor-pointer h-[100px]"
+            aria-label="Visit Supabase website"
+          >
+            <img
+              alt="Supabase"
+              loading="lazy"
+              width="40"
+              height="40"
+              decoding="async"
+              data-nimg="1"
+              className="z-10"
+              src="/supabase.svg"
+            />
+          </a>
+          <a
+            href="https://vercel.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="grid-item flex items-center justify-center border-b border-neutral-300 group cursor-pointer h-[100px]"
+            aria-label="Visit Vercel website"
+          >
+            <img
+              alt="Vercel"
+              loading="lazy"
+              width="70"
+              height="70"
+              decoding="async"
+              data-nimg="1"
+              className="z-10"
+              src="/vercel.svg"
+            />
+          </a>
+        </div>
         <div className="hidden lg:grid grid-rows-2">
           <div className="grid grid-cols-3 border-b border-neutral-300 h-[clamp(200px,20vw,400px)]">
             <a
